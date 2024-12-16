92d87050aca325245cd7ef2203b5383e8b73413c
Coffee374 <calvincaffee@seattleacademy.org>
2024-12-16 19:27:47 +0000

    miku for fun projects

---
+++ Projects/For_fun!/Hatsune_Miku_test.py
@@ -0,0 +1,8 @@
+while True:
+    word = input ("What do you think grandma likes?")
+
+    if word in ["cat", "dog", "rat"]:
+        print (f"Grandma dosen't like {word}!")
+    else:
+        print(f"Grandma likes {word}.")
+        
--- Projects/For_fun!/True_False_statement.py
+++
@@ -1,7 +0,0 @@
-while True:
-    word = input ("Hi i'm Hatsune Miku! Do you want to be my friend?")
-
-    if word in ["Yes"]:
-         print (f"Yay!")
-    else:
-        print (f"aww...")
