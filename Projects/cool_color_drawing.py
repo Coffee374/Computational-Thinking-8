65460526d069f2bbf5816335dcb3b2da65bab940
Coffee374 <calvincaffee@seattleacademy.org>
2025-01-14 18:01:36 +0000

    soren is so sigma with his color changeing squares

--- Projects/cool_color_drawing.py
+++ Projects/cool_color_drawing.py
@@ -1,18 +1,35 @@
+#makes the turtle come
 import turtle
 
+#does the background
+turtle.Screen().bgcolor("black")
+
+#sets up the pink turtle
 t = turtle.Turtle()
 t.speed(100)
-t.color("#F98B88")
+t.color("pink")
+t.left(20)
 
-t2 = turtle.Turtle
-t2.speed(99)
-t2.color("#87CEEB")
+#sets up the blue turtle
+t2 = turtle.Turtle()
+t2.speed(95)
+t2.color("skyblue")
 t2.left(10)
 
-for i in range(100000) :
-    t.forward(30 + i)
-    t.left(120 + 1)
-    t2.forward(30 + i)
-    t2.left(120 + 1)
+#sets up the white turtle
+t3 = turtle.Turtle()
+t3.speed(90)
+t3.color("white")
+t3.left(0)
 
+#makes the turtles do growing triangles
+for i in range(100000) :
+    t.forward(1 + i)
+    t.left(120 + 1)
+    t2.forward(1 + i)
+    t2.left(120 + 1)
+    t3.forward(1 + i)
+    t3.left(120 + 1)
+
+#kills the turtle?
 turtle.exitonclick()
---
+++ Projects/turtle_take_2.py
@@ -0,0 +1,11 @@
+#makes the turtle come
+import turtle
+
+#does the background
+turtle.Screen().bgcolor("black")
+
+#sets up the pink turtle
+t = turtle.Turtle()
+t.speed(100)
+t.color("pink")
+t.left(20)
