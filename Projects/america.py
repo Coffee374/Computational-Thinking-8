8ecbbc8541ce338cdc1c3417e30cd1c4bc22286c
Coffee374 <calvincaffee@seattleacademy.org>
2025-01-23 19:25:49 +0000

    sigma flag

--- Projects/america.py
+++ Projects/america.py
@@ -20,21 +20,17 @@ t2.penup()
 t2.goto(-350,300)
 t2.pendown()
 
+#sets up the star turtle (probably really shity)
+t3 = turtle.Turtle()
+t3.speed(100)
+t3.color("white")
+t3.penup()
+t3.goto(-300,200)
+t3.pendown()
 
-for i in range(10) :
-    for i in range(10) :
-        t.forward(700)
-        t.right(90)
-        t.forward(1)
-        t.right(90)
-        t.forward(700)
-        t.left(90)
-        t.forward(1)
-        t.left(90)
 
-    t.color("white")
-
-    for i in range(10) :
+for i in range(6) :
+    for i in range(20) :
         t.forward(700)
         t.right(90)
         t.forward(1)
@@ -46,7 +42,19 @@ for i in range(10) :
 
     t.color("red")
 
-for i in range(90) :
+    for i in range(20) :
+        t.forward(700)
+        t.right(90)
+        t.forward(1)
+        t.right(90)
+        t.forward(700)
+        t.left(90)
+        t.forward(1)
+        t.left(90)
+
+    t.color("white")
+
+for i in range(110) :
     t2.forward(300)
     t2.right(90)
     t2.forward(1)
@@ -55,5 +63,9 @@ for i in range(90) :
     t2.left(90)
     t2.forward(1)
     t2.left(90)
+
+for i in range(5):
+    t3.forward(50)
+    t3.left(45)
 
 turtle.exitonclick()
